2613cb3dba4d0f7b4b772f7392906d261ae06f06
tian969 <tian_zujie@Hotmail.com>
2023-07-31 23:16:50 +0800

    初版框架

---
+++ src/test/java/com/tzj/it/chatbotdemo/ChatBotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tzj.it.chatbotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
